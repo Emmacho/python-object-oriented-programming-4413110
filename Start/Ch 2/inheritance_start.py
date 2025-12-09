0486166fa11f7664559a2abc40c7e0585fb54daa
Emmacho <emmanuelayeleso@yahoo.com>
2025-12-09 13:50:32 -0500

    Refactor classes to implement abstract base classes and improve inheritance structure

--- Start/Ch 2/inheritance_start.py
+++ Start/Ch 2/inheritance_start.py
@@ -1,30 +1,35 @@
 # Python Object Oriented Programming by Joe Marini course example
 # Understanding class inheritance
 
-
-class Book:
-    def __init__(self, title, author, pages, price):
+class Publication:
+    def __init__(self, title, price):
         self.title = title
         self.price = price
+
+
+class Book(Publication):
+    def __init__(self, title, author, pages, price):
+        super().__init__(title, price)
         self.author = author
         self.pages = pages
 
-
-class Magazine:
+class Periodical(Publication):
     def __init__(self, title, publisher, price, period):
-        self.title = title
-        self.price = price
-        self.period = period
+        super().__init__(title, price)
         self.publisher = publisher
+        self.period = period
 
-
-class Newspaper:
+class Magazine(Periodical):
     def __init__(self, title, publisher, price, period):
-        self.title = title
-        self.price = price
-        self.period = period
-        self.publisher = publisher
+        super().__init__(title, publisher, price, period)
 
+       
+
+
+class Newspaper(Periodical):
+    def __init__(self, title, publisher, price, period):
+        super().__init__(title, publisher, price, period)
+        
 
 b1 = Book("Brave New World", "Aldous Huxley", 311, 29.0)
 n1 = Newspaper("NY Times", "New York Times Company", 6.0, "Daily")
--- Start/Ch 2/interface_start.py
+++ Start/Ch 2/interface_start.py
@@ -12,14 +12,25 @@ class GraphicShape(ABC):
     def calcArea(self):
         pass
 
+class JSONify(ABC):
+    def __init__(self):
+        super().__init__()
 
-class Circle(GraphicShape):
+    @abstractmethod
+    def toJSON(self):
+        pass
+
+
+class Circle(GraphicShape, JSONify):
     def __init__(self, radius):
         self.radius = radius
 
     def calcArea(self):
         return 3.14 * (self.radius ** 2)
+    def toJSON(self):
+        return f"{{'Circle': {str(self.calcArea())}}}"
 
 
 c = Circle(10)
 print(c.calcArea())
+print(c.toJSON())
